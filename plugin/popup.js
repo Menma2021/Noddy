67e1dc29979fb32e9efa9f6ef4de3e70137941a3
Caesar(Xuanpei_Chen) <13122531903@163.com>
2024-11-19 19:26:02 +0000

    change plugin

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,8 +1,9 @@
 let popupData = '';
 
-document.getElementById("changeColor").addEventListener("click", () => {
+document.getElementById("open_webpage").addEventListener("click", () => {
   // Get the URL of index.html
-  const indexUrl = chrome.runtime.getURL('webpage/index.html?data=' + popupData);
+  const selected_text = document.getElementById("selected_text").value;
+  const indexUrl = chrome.runtime.getURL('webpage/index.html?data=' + selected_text);
   // Open index.html in a new tab
   chrome.tabs.create({ url: indexUrl });
 });
@@ -10,7 +11,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'popup_data') {
     console.log('Data received in popup:', message.data);
     popupData = message.data;
+    document.getElementById("title").textContent = "Select Key";
+    document.getElementById("selected_text").value = popupData;
     // Display data in popup
-    
   }
 });
